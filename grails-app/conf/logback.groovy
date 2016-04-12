1e4c655a6f22453f291772f94edb9c30fc0ef639
Rahul Somasunderam <rsom@certifydatasystems.com>
2016-04-12 11:28:58 -0700

    Write logs to file
    SYSOUT is great, but sometimes it's easier to go into a file.

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,6 +1,8 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.FileAppender
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
 import grails.util.BuildSettings
 import grails.util.Environment
 
@@ -11,12 +13,23 @@ appender('STDOUT', ConsoleAppender) {
     }
 }
 
-root(ERROR, ['STDOUT'])
+def targetDir = BuildSettings.TARGET_DIR
+
+appender('FILE', RollingFileAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%date{HH:mm:ss.SSS} %5level [%15.15t] %25.25logger{5} - %msg%n"
+    }
+    rollingPolicy(TimeBasedRollingPolicy) {
+        FileNamePattern = "${targetDir}/log/esdemo-%d{yyyy-MM}.log.zip"
+    }
+    append = true
+}
+
+root(ERROR, ['STDOUT', 'FILE'])
 
 logger('esdemo', DEBUG)
 
 
-def targetDir = BuildSettings.TARGET_DIR
 if (Environment.isDevelopmentMode() && targetDir) {
     appender("FULL_STACKTRACE", FileAppender) {
         file = "${targetDir}/stacktrace.log"
