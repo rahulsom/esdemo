a8b57ae69de697b3d8be2255f150abdf9579c7dc
Rahul Somasunderam <rsom@certifydatasystems.com>
2016-04-12 11:30:30 -0700

    Create starting data for Merge

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,3 +1,4 @@
+import esdemo.PatientProcedurePerformed
 import grails.util.GrailsUtil
 
 import java.time.Instant
@@ -8,22 +9,35 @@ import static esdemo.Util.As
 class BootStrap {
 
     def init = { servletContext ->
-
         if (GrailsUtil.developmentEnv) {
-            def p1 = As('rahul') {
-                at(Instant.parse('2016-02-24T16:17:49.00Z'))
-                createPatient('123', '1.2.3.4', 'john')
-            }
-            As('donald') { changeName(p1, 'mike') }
-            As('daffy') { changeName(p1, 'sean') }
-            def nce = As('goofy') { changeName(p1, 'tim') }
-            As('goofy') { revertEvent(nce)}
-            As('micky') { planProcedure(p1, 'APPENDECTOMY')}
-            As('micky') { planProcedure(p1, 'FLUSHOT')}
-            As('stevie') { performProcedure(p1, 'FLUSHOT')}
-            As('micky') { planProcedure(p1, 'FLUSHOT')}
+            createJohn()
+            createBushes()
         }
+    }
 
+    private PatientProcedurePerformed createBushes() {
+        As('rahul') {
+            def b = createPatient('001', '1.2.3.4', 'george bush')
+            def w = createPatient('002', '1.2.3.4', 'george w bush')
+
+            planProcedure(b, 'ANNUAL')
+            performProcedure(w, 'ANNUAL')
+        }
+    }
+
+    private void createJohn() {
+        def p1 = As('rahul') {
+            at(Instant.parse('2016-02-24T16:17:49.00Z'))
+            createPatient('123', '1.2.3.4', 'john')
+        }
+        As('donald') { changeName(p1, 'mike') }
+        As('daffy') { changeName(p1, 'sean') }
+        def nce = As('goofy') { changeName(p1, 'tim') }
+        As('goofy') { revertEvent(nce) }
+        As('micky') { planProcedure(p1, 'APPENDECTOMY') }
+        As('micky') { planProcedure(p1, 'FLUSHOT') }
+        As('stevie') { performProcedure(p1, 'FLUSHOT') }
+        As('micky') { planProcedure(p1, 'FLUSHOT') }
     }
     def destroy = {
     }
