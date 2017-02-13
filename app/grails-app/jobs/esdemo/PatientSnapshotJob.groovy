2bfd841c1ddffab36594d2d9a97b4fab769e5e41
Rahul Somasunderam <rahul.som@gmail.com>
2017-02-13 14:38:22 -0800

    Update snapshot job

--- app/grails-app/jobs/esdemo/PatientSnapshotJob.groovy
+++ app/grails-app/jobs/esdemo/PatientSnapshotJob.groovy
@@ -1,34 +1,28 @@
 package esdemo
-
+import grails.compiler.GrailsCompileStatic
 class PatientSnapshotJob {
-
     def patientQueryUtil
-
     static triggers = {
         simple repeatInterval: 30000l
     }
-
+    @GrailsCompileStatic
     def execute() {
         log.debug "Starting..."
         def threshold = lastEventDate
         log.debug "Threshold is $threshold"
-
-        def events = lastEventDate ?
+        List<PatientEvent> events = lastEventDate ?
                 PatientEvent.findAllByDateCreatedGreaterThan(lastEventDate) :
                 PatientEvent.list()
-
         log.debug "Aggregating ${events.size()} events"
-
         def aggregates = events*.aggregate.unique()
         log.debug "Aggregating ${aggregates.size()} aggregates"
-
         aggregates.each {
-            def snapshot = patientQueryUtil.computeSnapshot(it, Long.MAX_VALUE)
-            log.debug "Id before save: ${snapshot.id}"
-            snapshot.save()
-            log.info "Id after save: ${snapshot.id}"
+            (patientQueryUtil as PatientQueryUtil).computeSnapshot(it, Long.MAX_VALUE).ifPresent { PatientSnapshot snapshot ->
+                log.debug "Id before save: ${snapshot.id}"
+                snapshot.save()
+                log.info "Id after save: ${snapshot.id}"
+            }
         }
-
         if (events) {
             def maxTime = events.dateCreated*.time.max()
             new File('build/date.txt').text = maxTime
@@ -37,7 +31,7 @@ class PatientSnapshotJob {
             log.debug "No new snapshots"
         }
     }
-
+    @GrailsCompileStatic
     Date getLastEventDate() {
         def dateFile = new File('build/date.txt')
         if (dateFile.exists() && dateFile.text.isLong()) {
